a8a8809227d3ff86f2e97dc81a38515c9827c685
Nick Peppers <nick.peppers@hitcents.com>
2017-05-12 11:53:00 -0500

    Android - fixed HtmlLabel and changed ListView to Recycle

--- TwitterSafari/TwitterSafari.Android/Renderers/HtmlLabelRenderer.cs
+++ TwitterSafari/TwitterSafari.Android/Renderers/HtmlLabelRenderer.cs
@@ -1,28 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
-using Xamarin.Forms.Platform.Android;
-using Xamarin.Forms;
+using System.ComponentModel;
 using TwitterSafari;
 using TwitterSafari.Droid;
-using Android.Text;
-using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 [assembly: ExportRenderer(typeof(HtmlLabel), typeof(HtmlLabelRenderer))]
 namespace TwitterSafari.Droid
 {
-    public class HtmlLabelRenderer : ViewRenderer<Label, TextView>
+    public class HtmlLabelRenderer : LabelRenderer
     {
-        TextView _textView;
-
         protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
         {
             base.OnElementChanged(e);
@@ -30,37 +17,11 @@ namespace TwitterSafari.Droid
             if (Element == null)
                 return;
 
-            if(Control == null)
+            if(Control != null)
             {
-                _textView = new TextView(Context);
-                SetHtmlText(Element.Text);
-                SetNativeControl(_textView);
-            }
-        }
-
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
-
-            if (Element == null || Control == null)
-                return;
-
-            if (e.PropertyName == HtmlLabel.TextProperty.PropertyName)
-            {
-                SetHtmlText(Element.Text);
-            }
-        }
-
-        private void SetHtmlText(string text)
-        {
-            
-            if (Android.OS.Build.VERSION.SdkInt >= BuildVersionCodes.N)
-            {
-                _textView.TextFormatted = Html.FromHtml(text, Android.Text.FromHtmlOptions.ModeCompact);
-            }
-            else
-            {
-                _textView.TextFormatted = Html.FromHtml(text);
+                Control.Clickable =
+                   Control.LinksClickable = true;
+                Control.AutoLinkMask = Android.Text.Util.MatchOptions.All;
             }
         }
     }
