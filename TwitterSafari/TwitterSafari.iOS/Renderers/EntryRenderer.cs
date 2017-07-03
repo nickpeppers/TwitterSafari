2f6e648a0e3f90b288a0c61995bf0e02132a98a8
nickpeppers <nicholas.peppers778@topper.wku.edu>
2017-07-03 15:59:02 -0400

    iOS - added EntryRenderer

---
+++ TwitterSafari/TwitterSafari.iOS/Renderers/EntryRenderer.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using System.ComponentModel;
+using TwitterSafari;
+using TwitterSafari.iOS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(iOSEntryRenderer))]
+namespace TwitterSafari.iOS
+{
+    public class iOSEntryRenderer : EntryRenderer 
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+                Control.BorderStyle = UIKit.UITextBorderStyle.None;
+        }
+
+    }
+}
