f6aa107be603f85a941501266999178a30280f2e
Nick Peppers <nick.peppers@hitcents.com>
2018-02-06 23:41:03 -0500

    Updated Packages and fixed UWP

---
+++ TwitterSafari/TwitterSafari.UWP/Services/SharingService.cs
@@ -0,0 +1,12 @@
+using LinqToTwitter;
+using TwitterSafari.Models;
+
+namespace TwitterSafari.UWP.Services
+{
+    public class SharingService : ISharingService
+    {
+        public void ShareTweet(Status status)
+        {
+        }
+    }
+}
